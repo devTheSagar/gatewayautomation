9e5f9b803025408647b2b399e395da679ad81e82
sagar biswas <sagarthewebdev@gmail.com>
2025-11-01 16:05:12 +0600

    updated

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
+use App\Models\Contact;
+use App\Models\ServedIndustries;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public static function index(){
-        
-        return view('backend.home');
+        $message = Contact::all();
+        $client = Client::all();
+        $service = Service::all();
+        $industries = ServedIndustries::all();
+        return view('backend.home', [
+            'message' => $message,
+            'client' => $client,
+            'service' => $service,
+            'industries' => $industries
+        ]);
     }
 }
