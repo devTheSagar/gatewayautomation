91ad636d02a6d9d157e643ab815b8baeb9d41b81
sagar biswas <sagarthewebdev@gmail.com>
2025-11-03 16:02:51 +0600

    updated

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
         $brochure = Brochure::first();
         $socialLink = SocialLink::first();
         $contactUs = ContactUs::first();
-        $services = Service::where('status', 1)->latest()->get();
+        $services = Service::where('status', 1)->orderBy('created_at', 'asc')->get();
 
         return view('frontend.home', [
             'carousels'         => $carousels,
